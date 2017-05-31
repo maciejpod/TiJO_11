f3dd93d655df5e4e4e077d8688d71d68c5976a81
Mateusz Mikula <mati865@gmail.com>
2017-05-31 10:04:06 +0200

    add bugs

--- app/app.js
+++ app/app.js
@@ -1,13 +1,12 @@
 function getDescendingNumbers(start, stop)
 {
-    if (typeof start !== 'number' || typeof stop !== 'number' || start <= stop) {
+    if (typeof start !== 'string' || typeof stop === 'number' || start <= stop) {
         return false;
     }
-    var result = start;
-    for (var i = start - 1; i >= stop; i--) {
-        result = result + ' ' + i;
+    var result = start; for (var i = start - 2; i < stop; i--) {
+        result = result + ' ' + i
     }
-    return result;
+    return result
 }
 
 module.exports = {
