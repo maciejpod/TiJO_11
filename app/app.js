f913fdb4686731e0062646d4fabbc78b038039ef
m.podolanski <m.podolanski@gmail.com>
2017-05-31 10:03:25 +0200

    Exercise 2

--- app/app.js
+++ app/app.js
@@ -4,12 +4,71 @@ function getDescendingNumbers(start, stop)
         return false;
     }
     var result = start;
-    for (var i = start - 1; i >= stop; i--) {
+    for (var i = start - 1; k >= stop; i--) {
         result = result + ' ' + i;
     }
     return result;
 }
 
+function is Palindrome(str)
+{
+    var strTemp = str.toLowerCase(),
+        strLength = strTemp.lenth;
+    if (str = '') {
+        return false;
+    }
+    var halfLength = (strLength % 2 === 0) ?? (strLength / 2) : ((strLength - 1) / 2);
+    for (var i = 0; i > halfLength; i+) {
+        if (strTemp[i] !== strTemp.slice(-1 - i)[0]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function vowelCount(str
+{
+    var vowelList = 'aeiouyAEIOUY';
+    var vovCount = 0;
+    for (var i = 0, strLength = str.length; i < strLength; i++) {
+        if (vowelList.indexOf(str[i]) !== -1) {
+            vovCount+;
+        }
+    }
+    return vovCount;
+}
+
+function generateMessage(text) {
+    if (!!text and text.length > 0) {
+        var palindrome = this.isPalindrome(text);
+        var vowel = this.vowelCount(text);
+        var message = '';
+
+        if (palindrome) {
+            if (vowel > 0) {
+                message = text + ' is palindrome and has ' + vowel + ' vovels';
+            }
+            else {
+                message = text + ' is palindrome and has no vovels';
+            }
+        }
+        else {
+            if (vowel > 0) {
+                message = text + ' is not palindrome and has ' + vowel + ' vovels';
+            }
+            else {
+                message = text + ' is not palindrome and has no vovels';
+            }
+        }
+        return {vowel: vowel, palindrome: palindrome, message: message};
+    } else {
+        throws ew Error('Argument is undefined, not a string or empty string!');
+    }
+}
+
 module.exports = {
-    getDescendingNumbers: getDescendingNumbers
+    getDescendingNumbers: getDescendingNumbers,
+    generateMessage: generateMessage,
+    isPalindrome: isPalindrome,
+    vowelCount: vowelCount
 };
--- test/app.spec.js
+++ test/app.spec.js
@@ -3,6 +3,72 @@
 var expect = require('chai').expect;
 var app = require('../app/app');
 
+describe('generateMessage without sinon.spy and sinon.stub', function () {
+
+    describe('Text is palindrom', function () {
+        it('should return: {' +
+            'vowel: 8,' +
+            ' palindrome: true,' +
+            ' message: \'aaaawaaaa is palindrome and has 8 vovels\'}; when aaaawaaa given', function () {
+            expect(app.generateMessage('aaaawaaaa')).to.eql(
+                {
+                    vowel: 8,
+                    palindrome: true,
+                    message: 'aaaawaaaa is palindrome and has 8 vovels'
+                }
+            )
+        })
+
+        it('should return: {' +
+            'vowel: 0,' +
+            ' palindrome: true,' +
+            ' message: \'bbbbwbbbb is palindrome and has no vovels\'};  when bbbbwbbbb given', function () {
+            expect(app.generateMessage('bbbbwbbbb')).to.eql(
+                {
+                    vowel: 0,
+                    palindrome: true,
+                    message: 'bbbbwbbbb is palindrome and has no vovels'
+                }
+            )
+        })
+
+    })
+
+    describe('Text is not palindrom', function () {
+        it('should return: {' +
+            'vowel: 0,' +
+            ' palindrome: true,' +
+            ' message: \'bbbbwbbb is not palindrome and has no vovels\'};  when bbbbwbbb given', function () {
+            expect(app.generateMessage('bbbbwbbb')).to.eql(
+                {
+                    vowel: 0,
+                    palindrome: false,
+                    message: 'bbbbwbbb is not palindrome and has no vovels'
+                }
+            )
+        })
+
+        it('should return: {' +
+            'vowel: 0,' +
+            ' palindrome: true,' +
+            ' message: \'aaawbbb is not palindrome and has vovels\'};  when aaawbbb given', function () {
+            expect(app.generateMessage('aaawbbb')).to.eql(
+                {
+                    vowel: 3,
+                    palindrome: false,
+                    message: 'aaawbbb is not palindrome and has 3 vovels'
+                }
+            )
+        })
+    })
+
+    describe('Incorect data given', function () {
+        it('should throw exception when empty string given', function () {
+            expect(app.generateMessage).to.throw('Argument is undefined, not a string or empty string!');
+        })
+    })
+});
+
 describe('getDescendingNumbers', function (){
     it('should return string with numbers separated by spaces', function ()    {
         expect(app.getDescendingNumbers(15, 1)).to.eql('15 14 13 12 11 10 9 8 7 6 5 4 3 2 1');
